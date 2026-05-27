a5b190a79fba672bf3a9a8ad5177b7aead18f835
pawunder1992 <pavelkryvutsa@gmail.com>
2026-05-27 13:06:37 +0300

    registed airoport models in admin panel

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Airport, Airplane, AirplaneType, Crew, Ticket, Flight, Order, Route
 
-# Register your models here.
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [TicketInline]
+
+
+
+admin.site.register(Airplane)
+admin.site.register(Airport)
+admin.site.register(AirplaneType)
+admin.site.register(Crew)
+admin.site.register(Flight)
+admin.site.register(Route)
+
+
+
--- airport/views.py
+++ airport/views.py
@@ -139,7 +139,6 @@ class OrderViewSet(viewsets.ModelViewSet):
         return queryset
 
 
-
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
